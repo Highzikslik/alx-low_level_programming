678b7b101e0836473908367772a93a1e4a066e8a
highzikslik <fashinaayodeleisaac@gmail.com>
2022-09-13 00:29:16 +0000

    Base16

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+/**
+ *  * main - prints out numbers using hexadecimal ascii values
+ *   *
+ *    * Return: Always 0 (Success)
+ *     */
+int main(void)
+{
+		int i;
+
+			for (i = 0x30; i <= 0x39; ++i)
+					{
+								putchar(i);
+									}
+				for (i = 0x61; i <= 0x66; ++i)
+						{
+									putchar(i);
+										}
+					putchar('\n');
+						return (0);
+}
