6b46d83df5d3b46e3ccdafb43d12cafbff6cc9b1
highzikslik <fashinaayodeleisaac@gmail.com>
2022-09-14 21:51:54 +0000

    100 times table

---
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -0,0 +1,50 @@
+#include "main.h"
+
+/**
+ * print_times_table - print times table of given integer
+ * @n: integer to be tested
+ */
+void print_times_table(int n)
+{
+	int x, y, product;
+
+	if (n >= 0 && n <= 15)
+	{
+		for (x = 0; x <= n; x++)
+		{
+			for (y = 0; y <= n; y++)
+			{
+				product = x * y;
+				if (y == 0)
+				{
+					_putchar(product + '0');
+				}
+				else if (product < 10 && y != 0)
+				{
+					_putchar(',');
+					_putchar(' ');
+					_putchar(' ');
+					_putchar(' ');
+					_putchar(product + '0');
+				}
+				else if (product >= 10 && product < 100)
+				{
+					_putchar(',');
+					_putchar(' ');
+					_putchar(' ');
+					_putchar((product / 10) + '0');
+					_putchar((product % 10) + '0');
+				}
+				else if (product >= 100)
+				{
+					_putchar(',');
+					_putchar(' ');
+					_putchar((product / 100) + '0');
+					_putchar(((product / 10) % 10) + '0');
+					_putchar((product % 10) + '0');
+				}
+			}
+			_putchar('\n');
+		}
+	}
+}
