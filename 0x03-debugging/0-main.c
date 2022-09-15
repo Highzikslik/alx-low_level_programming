b9fcb501a921986fc2fab38aac9499681ace50a6
highzikslik <fashinaayodeleisaac@gmail.com>
2022-09-15 14:09:45 +0000

    main bebug

--- 0x03-debugging/0-main.c
+++ 0x03-debugging/0-main.c
@@ -1,7 +1,7 @@
 #include "main.h"
 
 /**
- * tests function that prints if integer is positive or negative
+ * positive_or_negative - function prints if integer is positive or negative
  *
  * Return: 0
  */
