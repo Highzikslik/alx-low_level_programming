dc3bf4b461b1f684892f1dc5b510cea81b3c77d8
highzikslik <fashinaayodeleisaac@gmail.com>
2022-09-16 00:36:14 +0000

    print most numbers

---
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -0,0 +1,23 @@
+#include "main.h"
+
+/**
+ * print_most_numbers - prints num 0-9 except 2 & 4
+ * Return: no return value
+ */
+void print_most_numbers(void)
+{
+	int num;
+
+	for (num = 0; num <= 9; num++)
+	{
+		if ((num == 2) || (num == 4))
+		{
+			continue;
+		}
+		else
+		{
+			_putchar(num + '0');
+		}
+	}
+	_putchar('\n');
+}
